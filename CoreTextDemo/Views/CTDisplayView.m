080f1e6e06d0969a102e1a7bb00efadbf1e78024
Tang Qiao <tangqiao@fenbi.com>
2013-12-07 11:11:29 +0800

    test CGPathAddEllipseInRect effect.

--- CoreTextDemo/Views/CTDisplayView.m
+++ CoreTextDemo/Views/CTDisplayView.m
@@ -22,9 +22,13 @@
     CGContextScaleCTM(context, 1.0, -1.0);
     
     CGMutablePathRef path = CGPathCreateMutable();
-    CGPathAddRect(path, NULL, self.bounds);
+    CGPathAddEllipseInRect(path, NULL, self.bounds);
     
-    NSAttributedString *attString = [[NSAttributedString alloc] initWithString:@"Hello World!"];
+    NSAttributedString *attString = [[NSAttributedString alloc] initWithString:@"Hello World! "
+                                     "创建绘制的区域，CoreText本身支持各种文字排版的区域，"
+                                     "我们这里简单地将UIView的整个界面作为排版的区域。"
+                                     "为了加深理解，建议读者将该步骤的代码替换成如下代码，"
+                                     "测试设置不同的绘制区域带来的界面变化。"];
     CTFramesetterRef framesetter =
     CTFramesetterCreateWithAttributedString((CFAttributedStringRef)attString);
     CTFrameRef frame =
